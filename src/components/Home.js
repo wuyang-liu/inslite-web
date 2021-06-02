707c5bbddbb2310f6ad1a52786e88760c01b8b30
liuwuyayui <72761058+liuwuyayui@users.noreply.github.com>
2021-06-01 19:52:21 -0700

    add SearchBar.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,12 @@ function Home(props) {
     keyword: ''
   })
 
+  const handleSearch = (option) => {
+    const { type, keyword } = option;
+    setSearchOption({ type: type, keyword: keyword });
+  };
+
+
   useEffect(() => {
     const {type, keyword} = searchOption
     fetchPost(searchOption)
@@ -57,17 +63,41 @@ function Home(props) {
       return <div>No data!</div>;
     }
     if (type === "image") {
-      console.log("images -> ", posts);
-      return "images";
+      const imageArr = posts
+        .filter((item) => item.type === "image")
+        .map((image) => {
+          return {
+            src: image.url,
+            user: image.user,
+            caption: image.message,
+            thumbnail: image.url,
+            thumbnailWidth: 300,
+            thumbnailHeight: 200
+          };
+        });
+      return <PhotoGallery images={imageArr} />;
     } else if (type === "video") {
       console.log("video -> ", posts);
-      return "videos";
+      return (
+        <Row gutter={32}>
+          {posts
+            .filter((post) => post.type === "video")
+            .map((post) => (
+              <Col span={8} key={post.url}>
+                <video src={post.url} controls={true} className="video-block" />
+                <p>
+                  {post.user}: {post.message}
+                </p>
+              </Col>
+            ))}
+        </Row>
+      )
     }
   };
-
+  const operations = <Button>Upload</Button>
   return (
     <div className='home'>
-      <SearchBar/>
+      <SearchBar handleSearch={handleSearch}/>
       <div className='display'>
         <Tabs
           onChange={(key) => setActiveTab(key)}
--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,13 +1,117 @@
-import React, {Component} from 'react';
-import GridGallery from 'react-grid-gallery';
+import React, {Component, useState, useEffect} from 'react';
+import PropTypes from 'prop-types'
+import Gallery from 'react-grid-gallery';
+import {BASE_URL, TOKEN_KEY} from "../constants";
+import axios from "axios";
+import {message} from "antd";
+
+const captionStyle = {
+  backgroundColor: "rgba(0, 0, 0, 0.6)",
+  maxHeight: "240px",
+  overflow: "hidden",
+  position: "absolute",
+  bottom: "0",
+  width: "100%",
+  color: "white",
+  padding: "2px",
+  fontSize: "90%"
+}
+
+const wrapperStyle = {
+  display: "block",
+  minHeight: "1px",
+  width: "100%",
+  border: "1px solid #ddd",
+  overflow: "auto"
+}
 
 function PhotoGallery(props) {
+  const [images, setImages] = useState(props.images);
+  const [curImgIdx, setCurImgIdx] = useState(0);
 
-    return (
-      <div>
-        Gallery
-      </div>
-    );
+  const imageArr = images.map(image => {
+    return {
+      ...image,
+      customOverlay: (
+        <div style={captionStyle}>
+          <div>{`${image.user}: ${image.caption}`}</div>
+        </div>
+      )
+    }
+  });
 
+  const onDeleteImage = () => {
+    if (window.confirm(`Are you sure you want to delete this image?`)) {
+      const curImg = images[curImgIdx];
+      const newImageArr = images.filter((img, index) => index !== curImgIdx);
+      console.log('delete image ', newImageArr);
+      const opt = {
+        method: 'DELETE',
+        url: `${BASE_URL}/post/${curImg.postId}`,
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem(TOKEN_KEY)}`
+        }
+      };
+      axios(opt)
+        .then(res => {
+          console.log('delete result -> ', res);
+          // case1: success
+          if (res.status === 200) {
+            // step1: set state
+            setImages(newImageArr);
+          }
+        })
+        .catch(err => {
+          // case2: fail
+          message.error('Fetch posts failed!');
+          console.log('fetch posts failed: ', err.message);
+        })
+    }
+  }
+
+  const onCurrentImageChange = index => {
+    console.log('curIdx ', index);
+    setCurImgIdx(index)
+  }
+
+  useEffect(() => {
+    setImages(props.images)
+    setImages(props.images)
+  }, [props.images])
+
+
+  return (
+    <div style={wrapperStyle}>
+      <Gallery
+        images={imageArr}
+        enableImageSelection={false}
+        backdropClosesModal={true}
+        currentImageWillChange={onCurrentImageChange}
+        customControls={[
+          <Button style={{marginTop: "10px", marginLeft: "5px"}}
+                  key="deleteImage"
+                  type="primary"
+                  icon={<DeleteOutlined />}
+                  size="small"
+                  onClick={onDeleteImage}
+          >Delete Image</Button>
+        ]}
+      />
+    </div>
+  );
 }
+
+PhotoGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      user: PropTypes.string.isRequired,
+      caption: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+      thumbnail: PropTypes.string.isRequired,
+      thumbnailWidth: PropTypes.number.isRequired,
+      thumbnailHeight: PropTypes.number.isRequired,
+    })
+  ).isRequired
+};
+
 export default PhotoGallery;
